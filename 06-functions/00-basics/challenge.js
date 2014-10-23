bebe6134b7abf083241d7971c780c31f4e39d940
outofhand <rnoroian@gmail.com>
2014-10-23 13:42:18 +0000

    06-functions completed

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,10 @@
-module.exports.helloWorld = undefined;
+var helloWorld = function() {
+  return 'Hello World';
+}
 
-module.exports.multiply = undefined;
+var  multiply = function(x, y) {
+  return x*y;
+}
+
+module.exports.helloWorld = helloWorld;
+module.exports.multiply = multiply;  
