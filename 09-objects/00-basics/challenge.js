1543fea239bbb4f97910759673a3c97dd6efd633
outofhand <rnoroian@gmail.com>
2014-10-23 15:29:42 +0000

    09-objects completed

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,36 @@
-module.exports.createCourse = undefined;
+var createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle, 
+    duration: courseDuration,
+    students: courseStudents 
+  };
 
-module.exports.addProperty = undefined;
+  return course;
+};
 
-module.exports.formLetter = undefined;
+var addProperty = function(obj, prop, val) {
+  if ( !obj.hasOwnProperty(prop) ) {
+    obj[prop] = val;
+  } 
 
-module.exports.canIGet = undefined;
+  return obj;
+};
+
+var formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+var canIGet = function(item, money) {
+  var priceTable = {};
+  priceTable['MacBook Air'] = 999;
+  priceTable['MacBook Pro'] = 1299;
+  priceTable['Mac Pro'] = 2499;
+  priceTable['Apple Sticker'] = 1;       
+
+  return priceTable[item] <= money;
+};
+
+module.exports.createCourse = createCourse;
+module.exports.addProperty = addProperty;
+module.exports.formLetter = formLetter;
+module.exports.canIGet = canIGet;
